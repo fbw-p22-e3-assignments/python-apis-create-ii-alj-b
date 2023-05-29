1557db6fe3302aaf42ff6485e4afcfaf0cb2ce92
alj-b <aljoscha_b@icloud.com>
2023-05-29 23:58:37 +0200

    Imported UniqueTogetherValidator for simpler approach

--- dciproject/my_project/customer/serializers.py
+++ dciproject/my_project/customer/serializers.py
@@ -1,8 +1,9 @@
 #new - whole script added by instructor
 from rest_framework import serializers 
 from rest_framework.exceptions import ValidationError
+from rest_framework.validators import UniqueTogetherValidator
 from datetime import date
-from .models import Customer
+from .models import Customer      
 
 class CustomerSerializer(serializers.ModelSerializer):
 
