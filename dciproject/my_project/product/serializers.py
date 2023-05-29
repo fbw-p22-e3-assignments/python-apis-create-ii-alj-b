7142d200112f812f41ebff7bb7a4fcdf9ae6a43a
alj-b <aljoscha_b@icloud.com>
2023-05-30 00:01:35 +0200

    Imported UniqueTogetherValidator

--- dciproject/my_project/product/serializers.py
+++ dciproject/my_project/product/serializers.py
@@ -1,5 +1,6 @@
 #new - whole script added by instructor
 from rest_framework import serializers 
+from rest_framework.validators import UniqueTogetherValidator
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
