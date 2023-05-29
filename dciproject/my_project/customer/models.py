9897fc4ae365e6ff4529a40deae22890c9034131
alj-b <aljoscha_b@icloud.com>
2023-05-30 00:26:06 +0200

    Included sanitizer fields in Customer model

--- dciproject/my_project/customer/models.py
+++ dciproject/my_project/customer/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from sanitizer.models import SanitizedCharField, SanitizedTextField
+from sanitizer.models import SanitizedCharField
 
 # Create your models here.
 
 class Customer(models.Model): #new
-    name = models.CharField("Name", max_length=240)
+    name = models.SanitizedCharField("Name", max_length=240, allowed_tags=['a', 'p', 'input'])
     email = models.EmailField(unique=True)
     created = models.DateField(auto_now_add=True)
 
